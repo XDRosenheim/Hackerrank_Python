d4b71015a580e76ba8f30a8632bf93956fc66616
XDRosenheim_Server <patrickrosenheim@gmail.com>
2016-12-02 08:05:02 +0100

    Explanation README! _I will try to, at least_

--- Algorithms/Warmup/a-very-big-sum.py
+++ Algorithms/Warmup/a-very-big-sum.py
@@ -2,10 +2,4 @@
 
 n = int(input().strip())
 arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
-
-bigSum = 0
-
-for i in range(n):
-	bigSum += arr[i]
-
-print( bigSum )
+print(sum(arr))
